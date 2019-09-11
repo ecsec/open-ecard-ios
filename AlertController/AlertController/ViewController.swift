7e13ad7ff97bb2b3577be8e4773c113d6d2a916c
Florian Otto <florian.otto@ecsec.de>
2019-09-11 11:17:34 +0200

    Clean up app

--- AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/ViewController.swift
@@ -22,47 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func defaultAlertActionHandler(_ sender: UIButton) {
-        //Init AlertController
-        let alertController = UIAlertController.init(title: "Alert Title", message: "This is Alert Message", preferredStyle: .alert)
-        //Add action in AlertController
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            print("Ok Button Pressed")
-        }))
-        //Second way of Add Action in AlertController
-        //Init Alert Action
-        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
-            print("Cancel Button Pressed")
-        })
-        //This for add Alert Action into alertController
-        alertController.addAction(cancelButton)
-        
-        
-        //Show AlertController Via PresentViewController
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    @IBAction func defaultActionsheetActionHandler(_ sender: UIButton) {
-        //Init AlertController
-        let alertController = UIAlertController.init(title: "Actionsheet", message: "This is Action Message", preferredStyle: .actionSheet)
-        //Add action in AlertController
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            print("Ok Button Pressed")
-        }))
-        //Second way of Add Action in AlertController
-        //Init Alert Action
-        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
-            print("Cancel Button Pressed")
-        })
-        //This for add Alert Action into alertController
-        alertController.addAction(cancelButton)
-        
-        //Show AlertController Via PresentViewController
-        self.present(alertController, animated: true, completion: nil)
-    }
     
     
-    @IBAction func defaultAlertWithFormActionHandler(_ sender: UIButton) {
+    
+    @IBAction func defaultHandler(_ sender: UIButton) {
         doThingsWithFramework()
         
     }
