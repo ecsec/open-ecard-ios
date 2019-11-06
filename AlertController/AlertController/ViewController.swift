a8400a89b0a76981147dadd8670c9549cf4a2975
Florian Otto <florian.otto@ecsec>
2019-11-06 16:16:23 +0100

    Save

--- AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/ViewController.swift
@@ -9,11 +9,92 @@
 import UIKit
 import CoreNFC
 import OpenEcard
+import OpenEcard.open_ecard_mobile_lib
 
 class ViewController: UIViewController {
 
-    class ContextCompletion: NSObject, OpeneCardServiceHandlerProtocol{
+    class EacControllerStart: NSObject, ControllerCallbackProtocol, EacInteractionProtocol {
+        func onCardRecognized() {
+            print("on card recognized the wrong one")
+        }
+        
+        func onCardRecognized(_ msgHandler: (NSObjectProtocol & NFCOverlayMessageHandlerProtocol)!) {
+            print("on card recognized ")
+            msgHandler.setText("card recognized")
+            
+        }
+        
+        
+        let frm : OpenEcardProtocol;
+        init(frm : OpenEcardProtocol) {
+            self.frm = frm;
+            super.init()
+        }
+        
+        func onPinRequest(_ attempt: Int32, withEnterPin enterPin: (NSObjectProtocol & ConfirmPasswordOperationProtocol)!) {
+            print("onPinRequest");
+            enterPin.enter("123456")
+        }
+        
+        func onPinCanRequest(_ enterPinCan: (NSObjectProtocol & ConfirmTwoPasswordsOperationProtocol)!) {
+                print("onPinCanRequest");
+        }
+        
+        func onCardBlocked() {
+            print("blocked")
+        }
+        
+        func onCardDeactivated() {
+            
+        }
+        
+        func onServerData(_ data: (NSObjectProtocol & ServerDataProtocol)!, withSelectReadWrite selectReadWrite: (NSObjectProtocol & ConfirmAttributeSelectionOperationProtocol)!) {
+            
+            print("onServerData");
+        }
+        
+        func onTransactionInfo(_ data: String!) {
+            
+            print("onTransactionInfo");
+        }
+        
+        func onPinStatus(_ status: PinStatus, withCardType cardType: String!) {
+            
+        }
+        
+        func onInteractionComplete() {
+            
+            print("onInteractionComplete");
+        }
+        
+        func requestCardInsertion() {
+            
+            print("requestCardInsertion");
+        }
+        
+        func onCardRemoved() {
+            print("onCardRemoved");
+        }
+        
+        func onStarted() {
+            print("onStarted");
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.frm.triggerNFC()
+            }
+        }
+        
+        func onAuthenticationCompletion(_ result: (NSObjectProtocol & ActivationResultProtocol)!) {
+            print("onAuthenticationCompletion", result);        }
+        
+        
+    }
+    
+    class ContextCompletion: NSObject, StartServiceHandlerProtocol {
         var frm : OpenEcardProtocol? = nil;
+        var eacFactory : EacControllerFactoryProtocol? = nil;
+        var currentEacActivation: EacControllerStart? = nil;
+        var currentController: ActivationControllerProtocol? = nil;
        
         override init(){
         }
@@ -23,9 +104,19 @@ class ViewController: UIViewController {
             self.frm = frm;
         }
         
-        func onSuccess() {
+        func onSuccess(_ source: (NSObjectProtocol & ActivationSourceProtocol)!) {
             print("Context process completed successfully.")
-            frm!.triggerNFC()
+            
+            self.eacFactory = source.eacFactory();
+            self.currentEacActivation = EacControllerStart(frm: frm!);
+            print(eacFactory);
+            let baseUrl = "http://localhost/eID-Client?tcTokenURL="
+            let tcTokenUrl = "https%3A%2F%2Ftest.governikus-eid.de%3A443%2FAutent-DemoApplication%2FRequestServlet%3B%3Fprovider%3Ddemo_epa_20%26redirect%3Dtrue"
+            self.currentController = self.eacFactory?.create(baseUrl + tcTokenUrl, withActivation: currentEacActivation, with: currentEacActivation);
+            
+            // DispatchQueue.global(qos: .userInitiated).async {
+            //self.currentController?.start()
+            // }
             
         }
         
@@ -42,7 +133,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-     //  doSomethingWithOpeneCard()
+       doSomethingWithOpeneCard()
         //  doThingsWithProcessorFramework()
     }
 
@@ -61,18 +152,19 @@ class ViewController: UIViewController {
     }
 
     func doSomethingWithOpeneCard() {
+        print("Creating the framework");
         self.frm = createOpenEcard()
         
         print(frm)
-        
-        let context = frm?.context()
+        print("Creating the context");
+        let context = frm?.context("a", withDefaultNFCCardRecognizedMessage: "b")
+        print("Printing the contex");
         print(context)
         
-
         helper.setFrm(frm: frm!)
+        
+        print("Starting the openecard framework");
         context?.start(helper)
-
-
     }
     
     func doThingsWithProcessorFramework(){
