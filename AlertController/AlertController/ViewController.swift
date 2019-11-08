a879c998e9c8bc141d881d219ba6156c6156a481
Florian Otto <florian.otto@ecsec>
2019-11-08 10:27:54 +0100

    save

--- AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/ViewController.swift
@@ -14,9 +14,12 @@ import OpenEcard.open_ecard_mobile_lib
 class ViewController: UIViewController {
 
     class EacControllerStart: NSObject, ControllerCallbackProtocol, EacInteractionProtocol {
+        
         let frm : OpenEcardProtocol;
+        var msgHandler : NFCOverlayMessageHandlerProtocol?;
         init(frm : OpenEcardProtocol) {
             self.frm = frm;
+            self.msgHandler = nil;
             super.init()
         }
  
@@ -25,14 +28,11 @@ class ViewController: UIViewController {
         }
         
         func onServerData(_ data: (NSObjectProtocol & ServerDataProtocol)!, withTransactionData transactionData: String!, withSelectReadWrite selectReadWrite: (NSObjectProtocol & ConfirmAttributeSelectionOperationProtocol)!) {
-            
+            msgHandler?.setText("Allowing attributes automatically")
+            selectReadWrite.enter(data as? [NSObjectProtocol & SelectableItemProtocol], withWrite: nil)
         }
         
         func onCardAuthenticationSuccessful() {
-            
-        }
-        
-        func onCardInteractionComplete() {
             
         }
         
@@ -42,12 +42,14 @@ class ViewController: UIViewController {
         
         func onCardRecognized(_ msgHandler: (NSObjectProtocol & NFCOverlayMessageHandlerProtocol)!) {
             print("on card recognized ")
-            msgHandler.setText("card recognized")
+            msgHandler?.setText("card recognized")
+            self.msgHandler = msgHandler;
         }
        
         func onPinRequest(_ attempt: Int32, withEnterPin enterPin: (NSObjectProtocol & ConfirmPasswordOperationProtocol)!) {
-            print("onPinRequest");
-            enterPin.enter("123456")
+            let pin = "123123"
+            msgHandler?.setText("Entering pin: \(pin)")
+            enterPin.enter(pin)
         }
         
         func onPinCanRequest(_ enterPinCan: (NSObjectProtocol & ConfirmTwoPasswordsOperationProtocol)!) {
@@ -62,36 +64,32 @@ class ViewController: UIViewController {
             
         }
         
-        func onServerData(_ data: (NSObjectProtocol & ServerDataProtocol)!, withSelectReadWrite selectReadWrite: (NSObjectProtocol & ConfirmAttributeSelectionOperationProtocol)!) {
-            
-            print("onServerData");
+        func onCardInteractionComplete() {
+            print("onCardInteractionComplete")
         }
-        
         func onTransactionInfo(_ data: String!) {
-            
             print("onTransactionInfo");
         }
         
         func onInteractionComplete() {
-            
             print("onInteractionComplete");
         }
         
         func requestCardInsertion() {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.frm.triggerNFC()
+            }
             
-            print("requestCardInsertion");
         }
         
         func onCardRemoved() {
             print("onCardRemoved");
         }
         
+
+
+        //controller callback
         func onStarted() {
-            print("onStarted");
-            
-            DispatchQueue.global(qos: .userInitiated).async {
-                self.frm.triggerNFC()
-            }
         }
         
         func onAuthenticationCompletion(_ result: (NSObjectProtocol & ActivationResultProtocol)!) {
@@ -107,9 +105,6 @@ class ViewController: UIViewController {
         var currentEacActivation: EacControllerStart? = nil;
         var currentController: ActivationControllerProtocol? = nil;
        
-        override init(){
-        }
-        
       
         func setFrm(frm : OpenEcardProtocol){
             self.frm = frm;
@@ -120,7 +115,6 @@ class ViewController: UIViewController {
             
             self.eacFactory = source.eacFactory();
             self.currentEacActivation = EacControllerStart(frm: frm!);
-            print(eacFactory);
             let baseUrl = "http://localhost/eID-Client?tcTokenURL="
             let tcTokenUrl = "https%3A%2F%2Ftest.governikus-eid.de%3A443%2FAutent-DemoApplication%2FRequestServlet%3B%3Fprovider%3Ddemo_epa_20%26redirect%3Dtrue"
             self.currentController = self.eacFactory?.create(baseUrl + tcTokenUrl, withActivation: currentEacActivation, with: currentEacActivation);
@@ -133,7 +127,7 @@ class ViewController: UIViewController {
     }
     
     lazy var frm = createOpenEcard()
-    var helper = ContextCompletion()
+    var ctxCompletion = ContextCompletion()
    
     
     override func viewDidLoad() {
@@ -147,23 +141,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var tf2: UITextView!
     
     @IBAction func defaultHandler(_ sender: UIButton) {
-        doSomethingWithOpeneCard()
-    }
-
-    func doSomethingWithOpeneCard() {
-        print("Creating the framework");
-        self.frm = createOpenEcard()
-        
-        print(frm)
         print("Creating the context");
-        let context = frm?.context("a", withDefaultNFCCardRecognizedMessage: "b")
-        print("Printing the contex");
-        print(context)
-        
-        helper.setFrm(frm: frm!)
-        
-        print("Starting the openecard framework");
-        context?.start(helper)
+        let context = frm?.context("Please provide card", withDefaultNFCCardRecognizedMessage: "Found card")
+        ctxCompletion.setFrm(frm: frm!)
+        context?.start(ctxCompletion)
     }
 
 }
